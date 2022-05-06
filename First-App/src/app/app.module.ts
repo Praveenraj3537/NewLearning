4f45201c2a48e47c9d401eb916aea299e9b84024
Praveenraj <pattar.praveenraj3537@gmail.com>
2022-05-06 14:30:27 +0530

    Added required components

--- First-App/src/app/app.module.ts
+++ First-App/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateListComponent } from './create-list/create-list.component';
+import { ViewListComponent } from './view-list/view-list.component';
+import { ModifyListComponent } from './modify-list/modify-list.component';
+import { StatusComponent } from './status/status.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateListComponent,
+    ViewListComponent,
+    ModifyListComponent,
+    StatusComponent
   ],
   imports: [
     BrowserModule,
---
+++ First-App/src/app/modify-list/modify-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modify-list',
+  templateUrl: './modify-list.component.html',
+  styleUrls: ['./modify-list.component.scss']
+})
+export class ModifyListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
